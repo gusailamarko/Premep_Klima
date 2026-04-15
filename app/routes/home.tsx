c217839049f5fdf23c5d9edb665128bb2c85b533
Gusaila Márkó <gusailamarko10@gmail.com>
2026-04-15 22:02:19 +0200

    Vercel deployment error fixing

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import { OfferTexts } from '~/constants/texts';
-import {Hero, Intro, Pros, Offer, Process, TrustBuild, Faq} from '../../components/index';
+import {Hero, Intro, Pros, Offer, Process, Faq} from '../../components/index';
 
 const home = () => {
   return (
@@ -9,7 +9,6 @@ const home = () => {
       <Pros />
       <Offer title={OfferTexts.title} subtitle={OfferTexts.subtitle} listTitle={OfferTexts.listTitle} listItems={OfferTexts.listItems} finalWord={OfferTexts.finalWord} />
       <Process />
-      <TrustBuild />
       <Faq />
     </main>
   )
