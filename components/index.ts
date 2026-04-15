932676b3d1293f17169e0b47c50ea740bfa410d7
Gusaila Márkó <gusailamarko10@gmail.com>
2026-04-15 22:05:26 +0200

    Vercel deployment error fixing - error causing file remade

--- components/index.ts
+++ components/index.ts
@@ -4,4 +4,5 @@ export {default as Intro} from './Intro';
 export {default as Pros} from './Pros';
 export {default as Offer} from './Offer';
 export {default as Process} from './Process';
+export {default as TrustBuild} from './TrustBuild';
 export {default as Faq} from './Faq';
